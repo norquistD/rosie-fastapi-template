a7d576a70f6add6aa2b4aa137c5232c1f41da28f
Dylan Norquist <norquistdylan@gmail.com>
2024-11-23 01:56:33 -0600

    pushing up "readme" type of change

--- app/core/settings.py
+++ app/core/settings.py
@@ -13,18 +13,40 @@ class Settings(BaseSettings):
     SALT: str
 
     # Feel free to modify these for your platform
-    APP_NAME: str = "Rosie FastAPI Template"
-    APP_VERSION: str = "0.0.0"
+    APP_NAME: str = "Discovery World Hackathon API"
+    APP_VERSION: str = "0.1.0"
     APP_DESC: str = (
-        "A FastAPI template for building APIs on Rosie, the MSOE supercomputer. \nDeveloped by: Adam Haile - 2024"
+        """This is a FastApi that uses a template for building APIs on ROSIE, the MSOE supercomputer.
+        This Api for was created during a hackathon with Discovery World to increase inclusivity and accesibily.
+        It features are used to foster better translation, accessibility, and engagement:
+            - Translation into any language
+                - Text to Speech Translation
+                - Speech to Speech Translation
+                - Speech to Text Translation
+            - Formating Text to fit Environment
+                - Converts text to sound like a specified speaker
+            - Quiz Generation
+                - True or False Quizzes
+                - Multiple Choice Quizzes
+            - Chatting (In Developement)
+                - Answering Questions about Discovery World or that Experience's Topic
+
+        Discovery World Hackathon API Developed by: Dylan Norquist and Zander Ede - 2024
+        FastApi Template Developed by: Adam Haile - 2024
+        """
     )
 
-    ENVIRONMENT: str = "Rosie"
+    # OpenAI Models being used
     COMPLETIONS_MODEL: str = "gpt-4o-mini"
     AUDIO_MODEL:str = "whisper-1"
     TTS_MODEL: str = "tts-1"
-    TTS_MODEL_VOICE: str = "fable"
+    TTS_MODEL_VOICE: str = "fable" # This can and should be changed per a machine, possible as a parameter in the future
+    
+    # Api Keys
     OPENAI_API_KEY: str = ""
+    
+    # Environment Setup
+    ENVIRONMENT: str = "Rosie"
     DEVICE: str = "cuda"
     BASE_URL: str = ""
 
